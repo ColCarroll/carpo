c6ab27416d84dffd2a0a753e6485957b592d1c74
Colin Carroll <colcarroll@gmail.com>
2017-04-09 16:52:50 -0400

    Initial commit

---
+++ carpo/cli.py
@@ -0,0 +1,75 @@
+from collections import namedtuple
+import os
+import time
+
+import click
+import nbformat
+from nbconvert.preprocessors import ExecutePreprocessor
+
+from record import Records, get_default_home
+
+
+NotebookRun = namedtuple('NotebookRun', ['path', 'success', 'run_time', 'message'])
+
+@click.group()
+def cli():
+    """Base for cli commands."""
+    pass
+
+
+def execute_notebook(notebook_path):
+    """Run and overwrite a notebook file."""
+    executor = ExecutePreprocessor(timeout=-1)
+    with open(notebook_path, 'r') as buff:
+        notebook = nbformat.read(buff, as_version=nbformat.NO_CONVERT)
+    try:
+        start_time = time.time()
+        executor.preprocess(notebook, {'metadata': {'path': os.path.dirname(notebook_path)}})
+        run_time = time.time() - start_time
+
+    except KeyboardInterrupt:
+        raise
+
+    except BaseException as exc:
+        run_time = time.time() - start_time
+        success = False
+        msg = str(exc)
+    else:
+        with open(notebook_path, 'w') as buff:
+            nbformat.write(notebook, buff)
+        success = True
+        msg = ''
+    return success, run_time, msg
+
+
+def log_outcome(outcome):
+    """Log a notebook outcome with click.secho"""
+    if outcome.success:
+        color = 'green'
+        verb = 'succeeded'
+    else:
+        color = 'red'
+        verb = 'failed'
+
+    message = '{} {} in {:.1f}s'.format(os.path.basename(outcome.path), verb, outcome.run_time)
+    click.secho(message, fg=color)
+    if outcome.message:
+        click.secho(outcome.message, fg=color)
+
+
+@cli.command()
+@click.argument('notebooks', type=click.Path(), nargs=-1)
+@click.option('--db-file', default=get_default_home(),
+              help='Location to store results', show_default=True)
+@click.option('--force', is_flag=True, help='Run notebooks even if they have been run at this sha')
+def run(notebooks, db_file, force):
+    """Try to re-run all notebooks.  Print failures at end."""
+    records = Records(db_file)
+    if not force:
+        notebooks = [nb_path for nb_path in notebooks if not records.already_run(nb_path)]
+    for notebook_path in notebooks:
+        click.secho('Executing {}'.format(os.path.basename(notebook_path)))
+        success, run_time, msg = execute_notebook(notebook_path)
+        outcome = NotebookRun(notebook_path, success, run_time, msg)
+        records.record_outcome(outcome)
+        log_outcome(outcome)
---
+++ carpo/record.py
@@ -0,0 +1,110 @@
+import os
+import sqlite3
+import time
+
+from git import Repo, InvalidGitRepositoryError
+
+
+CREATE_SCHEMA = """
+    CREATE TABLE notebooks (
+        notebook_path TEXT,
+        git_sha TEXT,
+        git_root TEXT,
+        success INTEGER,
+        time NUMERIC,
+        run_date INTEGER)
+"""
+
+INSERT_SCHEMA = "INSERT INTO notebooks VALUES (?, ?, ?, ?, ?, ?)"
+
+
+def get_git_repo(notebook_path):
+    """If the notebook is checked into git, fetch the repository"""
+    try:
+        return Repo(notebook_path, search_parent_directories=True)
+    except InvalidGitRepositoryError:
+        return None
+
+
+def get_git_info(notebook_path):
+    """Fetch sha and root directory name for notebook, if it is checked into git"""
+    repo = get_git_repo(notebook_path)
+    if repo is None:
+        return '', ''
+    basename = os.path.basename(repo.working_dir)
+    sha = repo.commit().hexsha
+    return basename, sha
+
+def sort_git_shas(notebook_path, records, default_branch='master'):
+    """Sort a list of shas on a given branch."""
+    repo = get_git_repo(notebook_path)
+    if repo is None:
+        return records
+    commits = [c.hexsha for c in repo.iter_commits(default_branch)]
+    ordered, not_ordered = [], []
+    for record in records:
+        if record[1] in commits:
+            ordered.append(record)
+        else:
+            not_ordered.append(record)
+
+    return sorted(ordered, key=lambda record: commits.index(record.sha)), not_ordered
+
+def get_default_home():
+    """Use user's home directory as a default"""
+    return os.path.join(os.path.expanduser('~'), '.carpo.db')
+
+class DB(object):
+    def __init__(self, path):
+        self.path = path
+        self.conn = None
+        self.cursor = None
+
+    def __enter__(self):
+        self.conn = sqlite3.connect(self.path)
+        self.cursor = self.conn.cursor()
+        return self.cursor
+
+    def __exit__(self, exc_class, exc, traceback):
+        self.conn.commit()
+        self.conn.close()
+
+
+class Records(object):
+    def __init__(self, path):
+        self.path = path
+        self._db = DB(self.path)
+        self._create_table()
+
+    def _create_table(self):
+        if not os.path.exists(self.path):
+            with self._db as cur:
+                cur.execute(CREATE_SCHEMA)
+
+    def record_outcome(self, outcome):
+        """Insert a named tuple into the database"""
+        git_root, git_sha = get_git_info(outcome.path)
+        run_date = int(time.time())
+        result = (outcome.path, git_sha, git_root, int(outcome.success), outcome.run_time, run_date)
+        with self._db as cur:
+            cur.execute(INSERT_SCHEMA, result)
+
+    def already_run(self, notebook_path):
+        """Check if a notebook has already run under the given git sha"""
+        _, git_sha = get_git_info(notebook_path)
+        if not git_sha:
+            return False
+        with self._db as cur:
+            cur.execute("""SELECT 1 FROM notebooks
+                        WHERE success=1 AND notebook_path=? AND git_sha=? LIMIT 1""",
+                        (notebook_path, git_sha))
+            result = cur.fetchall()
+            print(bool(result))
+        return bool(result)
+
+    def list(self, notebook_path, branch='master'):
+        with self._db as cur:
+            cur.execute("SELECT * FROM notebooks WHERE success=1 AND notebook_path=?",
+                        (notebook_path,))
+            results = cur.fetchall()
+        return sort_git_shas(notebook_path, results, default_branch=branch)
